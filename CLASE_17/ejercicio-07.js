35ae1ef499263e60b244c5ace4bf74d3c7d2bdd7
Magamahe <magamahe@gmail.com>
2025-06-26 16:28:28 -0300

    ejercicios ultima clase 17

---
+++ CLASE_17/ejercicio-07.js
@@ -0,0 +1,7 @@
+/* Ejercicio 7: “La Floreria”
+Escribe una función "gestionarFloreria" que realice las siguientes tareas con una lista predefinida de nombres de flores:
+1. Quite los espacios en blanco alrededor de cada flor.
+2. Verifique si existe la flor "Margarita" y, si está presente, agregue "Azucena" al final de la lista.
+3. Reemplace todas las flores "Orquídea" por "Clavel".
+4. Encuentre el índice de la flor "Girasol" y, si no está presente, agregue "Girasol" al inicio de la lista.
+Devuelva una cadena de flores en orden alfabético separadas por puntos. */
---
+++ CLASE_17/ejercicio-09.js
@@ -0,0 +1,9 @@
+/* Ejercicio 9: Desafío extra! Orden, lugar y números
+Al ejercicio anterior vamos a aumentarle la dificultad un poco, y de paso, aplicamos lo aprendido en semanas anteriores. Quiero que hagan lo
+mismo del ejercicio anterior (buscar la posición de un número en un array), pero partiendo de esta lista:
+Para aplicar búsqueda binaria, deberán ordenar primero la lista, de menor a mayor, utilizando bubble sort. Luego, respondan las siguientes preguntas:
+o ¿Cuál es la posición del número 12?
+o ¿Cuál es la posición del número 5?
+o ¿Cuál es la posición del número 22?
+o ¿Cuál es la posición del número 100? */
+
