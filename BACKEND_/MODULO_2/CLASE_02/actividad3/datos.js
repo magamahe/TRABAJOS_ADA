56860018d7cb443e9c25c503cee867a6a6c85d72
Magamahe <magamahe@gmail.com>
2025-07-24 09:29:17 -0300

    se agrega clase2

---
+++ BACKEND_/MODULO_2/CLASE_02/actividad3/datos.js
@@ -0,0 +1,10 @@
+/*  En datos.js, define un objeto que tenga información como nombreCurso, duracion, y temas (array de cadenas).
+ */
+const curso ={
+    nombrecurso: 'IntroduccionJS',
+    duracion: 20,
+    temas: ['Node.js', 'Express', 'MongoDB','Js Avanzado']
+};
+
+// Exportamos el objeto curso para que pueda ser utilizado en otros archivos
+module.exports = curso;
